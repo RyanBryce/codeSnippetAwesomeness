8121c74d1e86dc4febc857dd66d0216e680b1a29
Ryan Bryce <ryan.bryce1992@gmail.com>
2019-01-20 21:58:49 -0700

    added some conditional rendering to nav bar to dynamically change the links

--- context-login/client/src/components/Nav.js
+++ context-login/client/src/components/Nav.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {Fragment}from 'react';
 import {Link} from 'react-router-dom';
+import { MyContext } from "../MyContext";
 
 const Nav = () => {
   return (
-    <ul className="nav justify-content-center">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">login</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/signup">signup</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/profile">profile</Link>
-      </li>
-    </ul>
+    <MyContext.Consumer>  
+      {(context)=> {
+        return(
+          <ul className="nav justify-content-center">
+            {
+              !context.state.loggedIn ? (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Login</Link>
+                </li>
+              ):(
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Logout</Link>
+                </li>
+              )
+            }
+            {
+              !context.state.loggedIn ? (
+                 <li className="nav-item">
+                  <Link className="nav-link" to="/signup">signup</Link>
+                </li>
+              ):(
+                <Fragment></Fragment>
+              )
+            }
+            <li className="nav-item">
+              <Link className="nav-link" to={`/profile/${context.state.username}`}>profile</Link>
+            </li>
+          </ul>
+        )
+      }}
+    </MyContext.Consumer>
   );
 };
 
